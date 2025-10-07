38d49f083d056c4b75cf37f384cc896debd77789
DenivaldoAntonio <160654856+DenivaldoAntonio@users.noreply.github.com>
2025-10-07 19:56:08 +0000

    aaa

--- lab4/scripts/main.js
+++ lab4/scripts/main.js
@@ -2,13 +2,45 @@
 
 let counter = 0;
 
-function count() {
+function countClick() {
    counter++;
    const soma = document.querySelector('#contador')
    soma.textContent = counter 
 } 
 
-function hello(){
-    const frase = document.querySelector('h1')
+function resetContador() {
+    counter = 0;
+    const reset = document.querySelector('#contador')
+    reset.textContent = counter
+}
 
+function destacarTitulo() {
+  const titulo = document.getElementById('titulo');
+  titulo.style.color = 'blue';
+  titulo.style.fontSize = '2rem';
+  titulo.textContent = 'Estás com o rato sobre o título!';
+}
+
+function voltarTitulo() {
+  const titulo = document.getElementById('titulo');
+  titulo.style.color = 'black';
+  titulo.style.fontSize = '1.5rem';
+  titulo.textContent = 'Explora os eventos de rato';
+}
+
+function mudarImagem() {
+  const mudar = document.querySelector('#imagem')
+  mudar.src = "https://picsum.photos/400/251"
+}
+
+function mudarTexto() {
+  const p = document.getElementById("paragrafo");
+  p.textContent = "JavaScript !!!";
+  p.style.color = "blue";
+}
+
+function voltarTexto() {
+  const p = document.getElementById("paragrafo");
+  p.textContent = "Move o rato sobre este parágrafo.";
+  p.style.color = "black";
 }
