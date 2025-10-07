7fbeff197dc2b02b074b01f152d2708eda7bde5b
DenivaldoAntonio <160654856+DenivaldoAntonio@users.noreply.github.com>
2025-10-07 16:38:22 +0000

    lab4

---
+++ lab4/scripts/main.js
@@ -0,0 +1,14 @@
+// main.js
+
+let counter = 0;
+
+function count() {
+   counter++;
+   const soma = document.querySelector('#contador')
+   soma.textContent = counter 
+} 
+
+function hello(){
+    const frase = document.querySelector('h1')
+
+}
